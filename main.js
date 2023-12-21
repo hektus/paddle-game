58ff7760220940346562d302cf20d9d0c98d9af3
Dawid Dzierzega <70227471+hektus@users.noreply.github.com>
2023-12-21 16:53:15 +0100

    add move player paddle functional (up and down)

--- main.js
+++ main.js
@@ -1,19 +1,27 @@
 const canvas = document.getElementById("canvas");
 const ctx = canvas.getContext("2d");
 
+//define up/down player move
+let playerUpPressed = false;
+let playerDownPressed = false;
+
 //define player paddle
 const playerPaddleSpeed = 5;
-const playerPaddleX = canvas.height / 2;
+let playerPaddleY = canvas.height / 2;
 
 //define AI paddle
 const aiPaddleSpeed = 5;
 const aiPaddleX = canvas.height / 2;
 
-//variables to both paddle and net
+//variables to both paddle and net(color)
 const paddleHeight = 150;
 const paddleWidth = 15;
 const paddleMarginOffset = 50;
 const paddleColor = "gold";
+
+//define ball
+const ballWidth = 50;
+const ballHeight = 50;
 
 //define net
 const netWidth = 5;
@@ -26,11 +34,27 @@ let playerScore = 0;
 //define aiScore
 let aiScore = 0;
 
+function keyUpHandler(e) {
+  if (e.key === 38 || e.key === "ArrowUp") {
+    playerUpPressed = false;
+  } else if (e.key === 40 || e.key === "ArrowDown") {
+    playerDownPressed = false;
+  }
+}
+
+function keyDownHandler(e) {
+  if (e.key === 38 || e.key === "ArrowUp") {
+    playerUpPressed = true;
+  } else if (e.key === 40 || e.key === "ArrowDown") {
+    playerDownPressed = true;
+  }
+}
+
 function drawPlayerPaddle() {
   ctx.beginPath();
   ctx.rect(
     paddleMarginOffset,
-    playerPaddleX - paddleHeight / 2,
+    playerPaddleY - paddleHeight / 2,
     paddleWidth,
     paddleHeight
   );
@@ -64,7 +88,7 @@ function drawMiddleNet() {
 
 function drawPlayerScore() {
   ctx.font = "28px Arial";
-  ctx.fillStyle = "yellow";
+  ctx.fillStyle = paddleColor;
   const textMetrics = ctx.measureText(`Player Score: ${playerScore}`);
   const textWidth = textMetrics.width;
   ctx.fillText(
@@ -76,7 +100,7 @@ function drawPlayerScore() {
 
 function drawAiScore() {
   ctx.font = "28px Arial";
-  ctx.fillStyle = "yellow";
+  ctx.fillStyle = paddleColor;
   const textMetrics = ctx.measureText(`Ai Score: ${aiScore}`);
   const textWidth = textMetrics.width;
   ctx.fillText(`Ai Score: ${aiScore}`, canvas.width - 300 - textWidth / 2, 30);
@@ -85,12 +109,31 @@ function drawAiScore() {
 //main function to draw all
 
 function draw() {
+  ctx.clearRect(0, 0, canvas.width, canvas.height);
   drawPlayerPaddle();
   drawAiPaddle();
   drawMiddleNet();
   drawPlayerScore();
   drawAiScore();
+
+  if (playerUpPressed) {
+    playerPaddleY -= playerPaddleSpeed;
+    if (playerPaddleY - paddleHeight / 2 < 0) {
+      playerPaddleY = paddleHeight / 2;
+    }
+  }
+
+  if (playerDownPressed) {
+    playerPaddleY += playerPaddleSpeed;
+    if (playerPaddleY + paddleHeight / 2 > canvas.height) {
+      playerPaddleY = canvas.height - paddleHeight / 2;
+    }
+  }
+
   requestAnimationFrame(draw);
 }
+
+document.addEventListener("keydown", keyDownHandler);
+document.addEventListener("keyup", keyUpHandler);
 
 draw();
